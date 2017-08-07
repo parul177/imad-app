cb2648c8b3c222572708c71b786b65ea24d857df
parul177 <30652899+parul177@users.noreply.github.com>
2017-08-07 23:12:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,10 @@
 console.log('Loaded!');
-
+//to change the text on the page
 var element= document.getElementById('main-text');
 element.innerHTML='new changed text';
+//to move the image
+var img=document.getElementById('madi');
+img.onclick=function()
+{
+    img.style.marginLeft='100px';
+};
