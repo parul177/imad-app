a670d793165e3a7ba8674e8f8ef883ca1ed9f87d
parul177 <30652899+parul177@users.noreply.github.com>
2017-08-09 13:47:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,9 @@
-console.log('Loaded!');
-//to change the text on the page
-var element= document.getElementById('main-text');
-element.innerHTML='new changed text';
-//to move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
+var button=document.getElementById('counter');
+var counter=0;
+button.click=function()
 {
-    marginLeft=marginLeft + 1;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,50);
+    counter++;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+    
 };
