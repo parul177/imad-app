ba207ba4f2ddcb3e24cf55c319d37a3ecff68998
parul177 <30652899+parul177@users.noreply.github.com>
2017-08-10 10:59:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,9 +1,22 @@
 var button=document.getElementById('counter');
-var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
-    
+    //create req object
+    var request=new XMLHttpRequest();
+    //capture the response & store it in a variable
+    request.onreadystatechange=function(){
+    if(request.readystate===XMLHttpRequest.DONE)
+    {
+        //take action
+        if(request.status===200)
+        {
+            var counter=request.responseText;
+             var span=document.getElementById('count');
+             span.innerHTML=counter.toString();
+        }
+    }
+    };
+    //make the request
+    request.open('GET','http://pscancer177.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
