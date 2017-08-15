e40ce306799aced4f906d3e2e3b3bde5eac4f974
parul177 <30652899+parul177@users.noreply.github.com>
2017-08-15 12:34:13 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config=
+{
+  user:'pscancer177',
+
+  database:'pscancer177',
+  
+host:'http://db.imad.hasura-app.io',
+  
+port:'54321',
+  
+password:process.env.DB_PASSWORD
+
+};
 
 
 var app = express();
@@ -86,6 +101,24 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+
+var pool=new Pool(config);
+
+app.get('/test_db', function (req, res) 
+{
+pool.query("SELECT * FROM test",function(err,result)
+  {
+  if(err)
+      {
+res.status(500).send(err.toString());
+      }
+  else
+      {
+         res.send(JSON.stringify(result));
+   }
+  });
 });
 
 app.get('/article-1', function (req, res) {
