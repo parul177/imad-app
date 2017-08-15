6b9b4973ab2a3d332fcfb5b841d393096cfbbdac
parul177 <30652899+parul177@users.noreply.github.com>
2017-08-15 14:11:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -130,7 +130,29 @@ app.get('/article-3', function (req, res) {
 
 
 
-
+app.get('/artcles/:articleName',function(req,res)
+{
+   pool.query("SELECT * FROM ARTICLE WHERE title=" +req.params.articleName,function(err,result)
+   {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }
+      else
+      {
+          if(result.rows.length===0)
+          {
+              res.status(404).send('article not found');
+          }
+          else
+          {
+              var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+          
+      }
+   }); 
+});
 
 
 app.get('/ui/main.js', function (req, res) {
