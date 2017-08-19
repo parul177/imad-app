55c2efae079e93b18be1430f61879c2dc15a0f2c
parul177 <30652899+parul177@users.noreply.github.com>
2017-08-19 16:20:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 
 var config=
 {
@@ -107,6 +108,21 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+function hash(input,salt)
+{
+    var hashed=crypto.pbkdf25(input,salt,10000,512,'sha512');
+
+    return hashed.toString('hex');
+}
+
+
+app.get('/hash/:input', function (req, res) {
+    var hashedstring=hash(req.params.input,'this-is-a-random-string');
+  res.send(hashedstring);
+});
+
 
 
 var pool=new Pool(config);
